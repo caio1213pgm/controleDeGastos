b5add9c729c64f866c9564d38e89937c39544290
Caio1213 <caiofxpgm@gmail.com>
2024-08-13 17:18:12 -0300

    Commit-verification-inputs
    HTML: add new div email and senha erro, change of onchage for oninput in the inputs.
    CSS: add and edit class erro.
    JS: delete validarCampo. add validarCampoEmail, validarCampoSenha, emaileValido, SenhAeValido, emailErro, senhaErro and botoesHabilitados. All thse functions are part of the verification inputs to know fields are valid

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
-function validarCampo() {
-    const email = document.getElementById('email').value;
+function validarCampoEmail() {
+    botoesHabilitados();
+    emailErro();
+}
 
+function validarCampoSenha(){
+    botoesHabilitados();
+    senhaErro();
+}
+
+function validarEmail(email){
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+function emaileValido(){
+    const email = document.getElementById('email').value;
     if(!email){
-        document.getElementById('btn-recuperar').disabled = true;
+        return false;
     }
-    else if(validarEmail(email)){
-        document.getElementById('btn-recuperar').disabled = false;
+    return validarEmail(email);
+}
+
+function senhAeValido(){
+    const senha = document.getElementById('senha').value;
+
+    if(!senha){
+        return false;
+    }
+    return true;
+}
+
+function emailErro(){
+    const email = document.getElementById('email').value;
+    if(!email){
+        document.getElementById('email-Obg-Erro').style.display = "block";
+    } 
+    else{
+        document.getElementById('email-Obg-Erro').style.display = "none";
+    }
+
+    if(validarEmail(email)){
+        document.getElementById('email-Inv-Erro').style.display = "none";
     }
     else{
-        document.getElementById('btn-recuperar').disabled = true;
-    }
-
-    function validarEmail(email){
-        return /\S+@\S+\.\S+/.test(email);
+        document.getElementById('email-Inv-Erro').style.display = "block";
     }
 }
 
+function senhaErro(){
+    const senha = document.getElementById('senha').value;
+
+    if(!senha){
+        document.getElementById('senha-Erro').style.display = "block";
+    }
+    else{
+        document.getElementById('senha-Erro').style.display = "none";
+    }
+}
+
+function botoesHabilitados(){
+    const emailValido = emaileValido();
+    document.getElementById('btn-recuperar').disabled = !emailValido;
+
+    const senhaValida = senhAeValido();
+    document.getElementById('btn-login').disabled = !emailValido || !senhaValida;
+}
